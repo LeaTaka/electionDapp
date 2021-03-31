06dd6831b7b2c87e0e214406ec4287e5e0139a1b
Jasper <jlhieselaar@jasconcept.com>
2021-03-31 23:02:16 +0200

    Initial commit

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.16;
+
+contract Election {
+    // store candidate
+    // read candidate
+    // Constructor
+    string public candidate; // has Getter function
+
+    constructor() public {
+        candidate = "Candidate 1"; // state variable
+    }
+
+}
